09c90c367eae434b853d0778a8102cd4d28a821d
Jonas Oberdan <jonasoberdan@gmail.com>
2026-02-07 01:08:30 -0300

    reoganazint folds

--- project/scripts/review.js
+++
@@ -1,5 +0,0 @@
-let count = localStorage.getItem("reviewCount") || 0;
-count++;
-localStorage.setItem("reviewCount", count);
-
-document.getElementById("reviewCounter").textContent = `Total Reviews Submitted: ${count}`;
